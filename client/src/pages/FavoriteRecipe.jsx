df7fc9131667b682b423bdbf7b64f7bfaf191d6f
fathanabds <abdulshodiqfathan@gmail.com>
2024-10-31 10:45:06 +0700

    feat: add no recipes data image message

--- client/src/pages/FavoriteRecipe.jsx
+++ client/src/pages/FavoriteRecipe.jsx
@@ -14,6 +14,14 @@ export default function FavoriteRecipe() {
   return (
     <>
       <h4 className="text-center mt-3">My Favorite Recipes</h4>
+
+      {recipes.length == 0 && (
+        <div className="text-center">
+          <img src="/no-data.png" alt="No Data" height={'200px'} />
+          <p>Your favorite recipes will appear here</p>
+        </div>
+      )}
+
       <div className="d-flex gap-1 flex-wrap justify-content-center my-3">
         {recipes.map((recipe) => {
           return <MyRecipeCard key={recipe.id} recipe={recipe} />;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,10 +4,13 @@ import Swal from 'sweetalert2';
 import spoonClient from '../helpers/spoonClient';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchMyRecipes } from '../features/myRecipesSlice';
+import { GridLoader } from 'react-spinners';
 
 export default function Home() {
   const myRecipes = useSelector((state) => state.myRecipes.recipes.value);
   const dispatch = useDispatch();
+
+  const [loading, setLoading] = useState(false);
 
   const [form, setForm] = useState({
     minCalories: '',
@@ -27,6 +30,7 @@ export default function Home() {
   async function fetchRecipes(form) {
     try {
       if (form.minCalories || form.maxCalories) {
+        setLoading(true);
         const { data } = await spoonClient.get(null, {
           params: {
             minCalories: form.minCalories || null,
@@ -34,6 +38,7 @@ export default function Home() {
           },
         });
         setRecipes(data);
+        setLoading(false);
       }
     } catch (error) {
       console.log(error);
@@ -61,7 +66,17 @@ export default function Home() {
           <span className="input-group-text">to</span>
           <input value={form.maxCalories} onChange={handleSearch} name="maxCalories" type="number" className="form-control" placeholder="Maximum calories: 800" />
         </div>
+        <div className="d-flex align-items-center justify-content-center my-3">
+          <GridLoader color={'#0B5ED7'} loading={loading} size={35} />
+        </div>
       </div>
+
+      {recipes.length == 0 && (
+        <div className="text-center">
+          <img src="/welcome-back.png" alt="No Data" height={'200px'} />
+          <p className="mt-3">Search recipes by your calorie needs</p>
+        </div>
+      )}
 
       <div className="d-flex gap-2 flex-wrap my-3 justify-content-center">
         {recipes.map((recipe) => {
